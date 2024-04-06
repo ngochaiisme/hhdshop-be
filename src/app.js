244f1529eb227d426e4eaa1df6923ba6c676551e
Dương Ngọc Hải <dngochai0511@gmail.com>
2024-04-06 16:23:53 +0700

    Add api products

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const product = require('./routes/product');
 
 const app = express();
 dotenv.config();
 
 //middleware
 app.use(cors());
-
+//route
+app.use('/api', product);
 //listening
 const PORT = process.env.PORT || 3000;
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port: ${PORT}...`)
-//     })
-// })
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}...`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}...`);
+    });
 });
