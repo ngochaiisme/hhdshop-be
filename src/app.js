86495b3bba548ec07eff9f1972f8428d7e99b95f
Dương Ngọc Hải <dngochai0511@gmail.com>
2024-05-18 15:29:04 +0700

    Add Homepage

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const product = require('./routes/product');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,17 @@ dotenv.config();
 app.use(cors());
 //route
 app.use('/api', product);
+
+// Middleware để xử lý root path
+app.use('/', (req, res, next) => {
+    // Kiểm tra nếu yêu cầu là tới root path
+    if (req.originalUrl === '/') {
+        // Trả về file index.html
+        return res.sendFile(path.join(__dirname, 'index.html'));
+    }
+    // Nếu không phải là root path, chuyển tiếp yêu cầu đến middleware hoặc route tiếp theo
+    next();
+});
 
 //listening
 const PORT = process.env.PORT || 3000;
